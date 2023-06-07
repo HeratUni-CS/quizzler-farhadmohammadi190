56a75750f3acca43f45dd224e3f2ef7b7e48fabc
farhad <farhadmohammadi5058@gmail.com>
2023-06-07 10:53:11 -0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
-//TODO: Step 2 - Import the rFlutter_Alert package here.
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
-
 QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
-
 class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
-          ),
-        ),
-      ),
-    );
+  return MaterialApp(
+  home: Scaffold(
+   backgroundColor: Colors.grey.shade900,
+    body: SafeArea(
+    child: Padding(
+   padding: EdgeInsets.symmetric(horizontal: 10.0),
+    child: QuizPage(),
+       ),),),);
   }
 }
 
@@ -34,4 +28,62 @@ class _QuizPageState extends State<QuizPage> {
 
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getCorrectAnswer();
+  
+  setState(() {
+      
+      if (quizBrain.isFinished() == true) {
+        
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+
+        quizBrain.reset();
+
+        scoreKeeper = [];
+      }
+
+      else {
+      if (userPickedAnswer == correctAnswer) {
+      scoreKeeper.add(Icon(
+      Icons.check,
+      color: Colors.green,
+          ));
+   } else {
+          scoreKeeper.add(Icon(
+   Icons.close,
+  color: Colors.red,
+    ));
+    }
+    quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+       Expanded(
+      flex: 5,
+      child: Padding(
+      padding: EdgeInsets.all(10.0),
+     child: Center(
+       child: Text(
+       quizBrain.getQuestionText(),
+       textAlign: TextAlign.center,
+   style: TextStyle(
+       fontSize: 25.0,
+      color: Colors.white,
+                ),),),), ),
+  
+  
+  
+  
+  
+  
+  
   }}
